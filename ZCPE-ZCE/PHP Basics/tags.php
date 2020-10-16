8662de327be4c58f2d6e2803b3142d47eda843fd
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-10-16 01:37:41 -0300

    tags.php

---
+++ ZCPE-ZCE/PHP Basics/tags.php
@@ -0,0 +1,11 @@
+<?php echo "extended tag"; ?>
+
+<!-- sudo gedit /etc/php/php.ini -->
+
+<!-- short_open_tag = Off -->
+
+<!-- sudo systemctl restart httpd -->
+
+<?= "short tag" ?>
+
+<? echo "short tag" ?>
