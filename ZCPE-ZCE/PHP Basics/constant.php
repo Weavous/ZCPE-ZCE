80cda5527552ec309e2bb9b666de1c7a2e1cd1f7
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-10-17 23:21:56 -0300

    constant.php

---
+++ ZCPE-ZCE/PHP Basics/constant.php
@@ -0,0 +1,17 @@
+<?php
+    echo(__LINE__);
+
+    echo(__FILE__);
+
+    echo(__DIR__);
+
+    echo(__FUNCTION__);
+
+    echo(__CLASS__);
+
+    echo(__TRAIT__);
+
+    echo(__METHOD__);
+
+    echo(__NAMESPACE__);
+?>
