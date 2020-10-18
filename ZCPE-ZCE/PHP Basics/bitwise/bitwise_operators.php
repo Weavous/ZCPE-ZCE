bcedb1c99ad3a6fcf25bb08a2f0d1107eaad6331
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-10-17 23:01:15 -0300

    bitwise_operators.php

---
+++ ZCPE-ZCE/PHP Basics/bitwise/bitwise_operators.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "left_shift_bitwise.php";
+
+require_once "right_shift_bitwise.php";
+
+require_once "not.php";
+
+require_once "and.php";
+
+require_once "or.php";
+
+require_once "xor.php";
